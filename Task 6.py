614fd777cecbfdc702c43f6578c6503bba785e29
wow <gegulay@gmail.com>
2020-08-30 18:18:44 +0300

    The minimum element of the matrix.

--- Task 6.py
+++ Task 6.py
@@ -16,3 +16,11 @@ for i in range(m):
         if matrix[i][j] > maximum:
             maximum = matrix[i][j]
 print(maximum)
+
+minimum = matrix[0][0]
+
+for i in range(m):
+    for j in range(n):
+        if matrix[i][j] < minimum:
+            minimum = matrix[i][j]
+print(minimum)
